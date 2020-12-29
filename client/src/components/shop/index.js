53c303d75759c2b4c70f976d11ae1009a253e724
unknown <msavantfou@gmail.com>
2020-12-30 00:23:10 +1100

    half way through svg animation

--- client/src/components/shop/index.js
+++ client/src/components/shop/index.js
@@ -13,6 +13,8 @@ function Index() {
   const [shops, setShop] = useState([]);
   const [colors, setColor] = useState({});
   const [shopColor, setShopColor] = useState({});
+  const [redArr, setRed] = useState([]);
+  const [yellowArr, setYellow] = useState([]);
   const [zero, setZero] = useState([]);
   const [closeContacts, setClose] = useState([]);
   const [secondShops, setSecShop] = useState([]);
@@ -23,6 +25,7 @@ function Index() {
   const init = () => {
     const people = []
     const shopArr = []
+    const colorArr = {}
     for (let i = 0; i < 2000; i++) {
       const num = '0' + (500000000 + i);
       const x = Math.ceil(Math.random() * 1450 + 30)
@@ -34,7 +37,9 @@ function Index() {
       const x = Math.ceil(Math.random() * 1450 + 20)
       const y = Math.ceil(Math.random() * 800 + 20)
       shopArr.push({ x, y, num, icon: genRandom(icons) })
+      colorArr[num] = { color: '', lines: [] }
     }
+    setShopColor(colorArr)
     setShop(shopArr)
     setPosition(people)
 
@@ -45,17 +50,28 @@ function Index() {
   }
 
   const reset = () => {
-    const arr = document.getElementsByClassName('shop');
-    console.log(arr.length)
-    for (let i = 0; i < arr.length; i++) {
-      arr[i].classList.remove('red', 'yellow', 'primary')
-    }
+    // const arr = document.getElementsByClassName('normal');
+    // for (let i = 0; i < arr.length; i++) {
+    //   arr[i].classList.remove('red', 'yellow', 'primary');
+    //   arr[i].style.color = 'rgba(17, 186, 238, 0.315)';
+    //   arr[i].style.border = 'rgba(17, 186, 238, 0.315) solid 2px';
+    // }
     setZero([])
     setClose([])
     setSecShop([])
     setSecCont([])
-    setShopColor({})
+    const tempColArr = {}
+    shops.forEach(shop => {
+      tempColArr[shop.num] = { lines: [], color: '' }
+    })
+    setShopColor(tempColArr)
     setColor({});
+    setLineDisplay({
+      display1: 'none',
+      display2: 'none',
+      display: 'block',
+      text: 'Show Line'
+    })
     axios.get('/api').then(res => console.log(res));
   }
 
@@ -80,11 +96,13 @@ function Index() {
         const arrShops2 = res.data.secondaryShops
         const primeContact = res.data.primaryContacts;
         const color = colors
-        const shopCol = shopColor
+        const colorShop = shopColor
         const lineZero = zero;
-        const lineClose = closeContacts;
+        const lineClose = [];
         const lineShop = secondShops;
         const lineSecond = secondContacts;
+        const tempRed = [];
+        const tempYellow = [];
 
         primeContact.forEach((element, index) => {
           const shop = document.getElementById(element.phoneNumber);
@@ -92,42 +110,47 @@ function Index() {
           const x0 = (right + left) / 2
           const y0 = (top + bottom) / 2
           element.customersToTag.forEach((customer, i) => {
-            console.log('logging customer: ', customer)
             const xy = document.getElementById(customer);
             const { bottom, top, right, left } = xy.getBoundingClientRect();
             const x1 = (right + left) / 2;
             const y1 = (bottom + top) / 2;
             if (customer === name) {
-              lineZero.push({ x0: x1, y0: y1, x1: x0, y1: y0 })
+              lineZero.push({ x0: x1, y0: y1, x1: x0, y1: y0, end: element.phoneNumber })
               setZero(lineZero);
             } else {
-              lineClose.push({ x0, y0, x1, y1 })
-              setClose(lineClose)
+              lineClose.push({ x0, y0, x1, y1, end: customer })
             }
           })
-          shopCol[element.phoneNumber] = shopRed;
+          // colorShop[element.phoneNumber].color = shopRed;
+          colorShop[element.phoneNumber].lines = lineClose;
+          tempRed.push(element.phoneNumber)
         });
 
         arrShops2.forEach((element, index) => {
           const shop = document.getElementById(element.phoneNumber);
-          if (shopCol[element.phoneNumber] !== shopRed) {
-            shopCol[element.phoneNumber] = shopYellow
-          }
+          // if (colorShop[element.phoneNumber].color !== shopRed) {
+          //   colorShop[element.phoneNumber].color = shopYellow
+          //   tempYellow.push(element.phoneNumber)
+          // }
         });
         arr.forEach(element => {
           if (element === name) {
             color[element] = primary
           } else if (color[element] !== primary) {
             color[element] = red
+            tempRed.push(element)
           }
         });
         arr2.forEach(element => {
           if (color[element] !== red && color[element] !== primary) {
             color[element] = yellow
+            tempYellow.push(element)
           }
         });
-        setColor(color)
-        setShopColor(shopCol)
+        // setColor(color)
+        setRed(tempRed);
+        setYellow(tempYellow);
+        setShopColor(colorShop)
       })
   }
   const test = (e) => {
@@ -141,33 +164,54 @@ function Index() {
   }
   const [lineDisplay, setLineDisplay] = useState({
     display: 'block',
+    display1: 'block',
+    display2: 'none',
     text: 'Clear Line'
   })
   const clearLine = () => {
     if (lineDisplay.display !== 'none') {
       setLineDisplay({
+        display1: 'none',
+        display2: 'none',
         display: 'none',
         text: 'Show Line'
       })
     } else {
       setLineDisplay({
+        display1: 'block',
+        display2: 'block',
         display: 'block',
         text: 'Clear Line'
       })
     }
 
   }
-  const firstshops = () => {
-
+  const firstStop = (e) => {
+    console.log(e)
+    const colObj = shopColor;
+    const name = e.target.getAttribute('name')
+    colObj[name].color = shopRed
+    setShopColor(colObj);
+    const lines = secondShops.concat(shopColor[name].lines);
+    setSecShop(lines);
+  }
+  const secondStop = (e) => {
+    const name = e.target.getAttribute('name')
+    const colObj = colors;
+    colObj[name] = red
+    setColor(colors => ({ ...colors, ...colObj }))
+    // const node = document.getElementById(name);
+    // node.style.color = 'rgba(255, 0, 0, 0.644)'
+    // node.style.border = 'rgba(255, 0, 0, 0.644) solid 2px';
   }
   return (
     <div>
       <svg style={{ display: `${lineDisplay.display}` }}>
         {zero.map((line, index) => (
-          <line key={index} onAnimationEnd={firstshops} x1={line.x0} y1={line.y0} x2={line.x1} y2={line.y1} style={{ stroke: 'rgb(255,0,0)', strokeWidth: '1px' }} />
+          <line key={index} name={line.end} onAnimationEnd={firstStop} x1={line.x0} y1={line.y0} x2={line.x1} y2={line.y1} style={{ stroke: 'rgb(255,0,0)', strokeWidth: '1px' }} />
         ))}
-        {closeContacts.map((line, index) => (
-          <line key={index} x1={line.x0} y1={line.y0} x2={line.x1} y2={line.y1} style={{ stroke: 'rgb(255,0,0)', strokeWidth: '1px' }} />
+        {secondShops.map((line, index) => (
+          <line onAnimationEnd={secondStop} name={line.end} key={index} x1={line.x0} y1={line.y0} x2={line.x1} y2={line.y1} style={{ stroke: 'rgb(255,0,0)', strokeWidth: '1px' }} />
         ))}
         {/* {contactLines.map((line, index) => (
           <line key={index} x1={line.x0} y1={line.y0} x2={line.x1} y2={line.y1} style={{ stroke: 'rgb(255,0,0)', strokeWidth: '1px' }} />
@@ -188,7 +232,7 @@ function Index() {
           </div>
         ))}
         {shops.map((shop, index) => (
-          <div key={index} className={shopColor[shop.num] || 'shop'}
+          <div key={index} className={shopColor[shop.num].color || 'shop'}
             id={shop.num}
             style={{ top: `${shop.y}px`, left: `${shop.x}px` }}
             // onMouseOver={test}
